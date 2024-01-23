f82067bacf0f67a80546b9cd8e522adbfa7c6c90
rutvisha <rutvishapatel9@gmail.com>
2024-01-23 21:30:47 +0000

    rutu

---
+++ src/main/java/com/restful/constants/Path.java
@@ -0,0 +1,15 @@
+package com.restful.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+
+    /*
+     * This is base path for Booking api
+     */
+    public static final String BOOKING = "/booking";
+    public static final String AUTHORIZATION = "/auth";
+
+
+}
